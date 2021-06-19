b3c8b0339ca982317fafc83ab9f0852d12501c6e
Sandeep Kumar <57530878+sandeep2559@users.noreply.github.com>
2021-06-19 22:13:09 +0530

    created Procfile updated port and updated package.json

--- news.js
+++ news.js
@@ -65,8 +65,14 @@ app.post("/failure",function(req,res){
 })
 
 
-app.listen(process.env.PORT||4000,function(){
-    console.log("server 4000 is started on the port");
+let port=process.env.PORT;
+if(port==null || port==""){
+  port=3000;
+}
+
+
+app.listen(port, function () {
+  console.log("Server started on port 3000");
 });
 
 
